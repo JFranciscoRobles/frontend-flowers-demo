3b16fb0e8b2eb22a9b5b8e0e7140eced91446bc3
JFranciscoRobles <josefrancisco.roblesacosta@gmail.com>
2024-05-30 10:32:22 -0700

    fixlinter

--- src/components/ui/link.tsx
+++ src/components/ui/link.tsx
@@ -1,23 +1,25 @@
-import { AnchorHTMLAttributes } from "react";
-import Link from "next/link";
-import { cn } from "@/lib/utils";
+import { AnchorHTMLAttributes, FC } from 'react'
+import Link from 'next/link'
+import { cn } from '@/lib/utils'
 
-type LinkPorpps = AnchorHTMLAttributes<HTMLAnchorElement>;
+type InternalLinkProps = AnchorHTMLAttributes<HTMLAnchorElement> & {
+  href: string
+}
 
-const LinkUI: React.FC<LinkPorpps> = (props) => {
+type LinkProps = InternalLinkProps & { href: string }; // Update type to match InternalLinkProps and ensure href is always defined
+
+const LinkUI: FC<LinkProps> = (props) => {
   return (
     <Link
       {...props}
-      //@ts-ignore
-      name="link"
-      //@ts-ignore
+      href={props.href} // Ensure href is always defined
       className={cn(
-        "inline-flex justify-center items-center p-2 text-sm leading-none text-center rounded-lg ring-opacity-30 hover:bg-muted hover:shadow-outline focus:outline-none focus:ring-2 ring-primary/40 focus:bg-primary/20"
+        'inline-flex justify-center items-center p-2 text-sm leading-none text-center rounded-lg ring-opacity-30 hover:bg-muted hover:shadow-outline focus:outline-none focus:ring-2 ring-primary/40 focus:bg-primary/20'
       )}
     >
       {props.children}
     </Link>
-  );
-};
+  )
+}
 
-export default LinkUI;
+export default LinkUI
